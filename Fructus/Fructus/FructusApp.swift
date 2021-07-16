80b5b7c616fdc1f4256c637cca7a3e96efecbd2f
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2021-07-16 19:58:01 +0200

    create new project

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Zdenko Čepan on 16/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
