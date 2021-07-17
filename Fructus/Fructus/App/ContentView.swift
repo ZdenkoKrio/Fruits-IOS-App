88189235a8911d75330c9b812a69d03843ec7005
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2021-07-17 12:46:39 +0200

    created navigationList of fruits

--- Fructus/Fructus/App/ContentView.swift
+++ Fructus/Fructus/App/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: Properties
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: Body
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { item in
+                    FruitRowView(fruit: item)
+                        .padding(.vertical, 4)
+                } //LOOP
+            } // LIST
+            .navigationTitle("Fruits")
+        } // NAVIGATION
     }
 }
 
+// MARK: Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
+            .previewDevice("iPhone 11 Pro")
     }
 }
